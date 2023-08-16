7813bfba83b0e84951462b56b6a2b0522e3cde8b
franpa13 <frandelregue@gmail.com>
2023-08-16 14:16:49 -0300

    implementando modelo mvc
    agregue dos carpetas , una de controllers y otra de routers

---
+++ routers/main.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+const mainController = require("../controllers/mainController")
+router.get("/",mainController.renderHome)
+router.get("/login",mainController.renderLogin)
+router.get("/register",mainController.renderRegister)
+
+module.exports = router
